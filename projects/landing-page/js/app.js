8903f0713c0faef7d90cd7c0ae7107583867b9b0
Joseph Johnson <johnson_jt@outlook.com>
2022-04-14 18:58:36 -0400

    Update app.js styling

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -11,17 +11,17 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 const sections = Array.from(document.getElementsByTagName("section"));
 
 
@@ -29,7 +29,7 @@ const sections = Array.from(document.getElementsByTagName("section"));
  * End Global Variables
  * Start Helper Functions
  *
-*/
+ */
 //Function will determine if current element is within the viewport
 function activeViewPort(element) {
     const rect = element.getBoundingClientRect();
@@ -45,7 +45,9 @@ function activeViewPort(element) {
 const scroll = (item, section) => {
     item.addEventListener("click", function(e) {
         e.preventDefault();
-        section.scrollIntoView({behavior: "smooth"});
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
     })
 }
 
@@ -70,14 +72,14 @@ const collapse = (section) => {
  * End Helper Functions
  * Begin Main Functions
  *
-*/
+ */
 
 // build the nav
 function buildNavBar() {
     const navBarMenu = document.querySelector('.navbar__menu');
     const navBarList = document.getElementById('navbar__list');
 
-    for (section of sections ) {
+    for (section of sections) {
         const item = document.createElement('li');
         item.innerHTML += `<a class="menu__link" data=${section.id} href=${section.id}>${section.dataset.nav}</a>`;
         navBarList.appendChild(item);
@@ -92,14 +94,14 @@ buildNavBar();
 
 // Add class 'active' to section
 document.addEventListener("scroll", function() {
+    e.preventDefault;
     for (let i = 0; i < sections.length; i++) {
         let section = sections[i];
 
         if (activeViewPort(section)) {
             section.classList.add("active-class");
             document.querySelector(`[data=${section.id}]`).classList.add('active-nav');
-        }
-        else {
+        } else {
             section.classList.remove("active-class");
             document.querySelector(`[data=${section.id}]`).classList.remove('active-nav');
         }
@@ -108,19 +110,21 @@ document.addEventListener("scroll", function() {
 
 //Hides the navigation bar when scrolling
 const lastScrollTop = 0;
-window.addEventListener("scroll", function(){
-   const location = window.pageYOffset;
-   if (location > lastScrollTop + 20){
-       document.getElementsByClassName("page__header")[0].style.display = "none";
-   } else {
-      document.getElementsByClassName("page__header")[0].style.display = "inline-block";
-   }
-   lastScrollTop = location;
+window.addEventListener("scroll", function() {
+    e.preventDefault;
+    const location = window.pageYOffset;
+    if (location > lastScrollTop + 20) {
+        document.getElementsByClassName("page__header")[0].style.display = "none";
+    } else {
+        document.getElementsByClassName("page__header")[0].style.display = "inline-block";
+    }
+    lastScrollTop = location;
 });
 
 //Scrolls to top when clicked
 const tb = document.getElementById("topButton");
 tb.addEventListener("click", function(e) {
+    e.preventDefault;
     window.scrollTo({
         top: 0,
         behavior: "smooth"
@@ -131,4 +135,4 @@ tb.addEventListener("click", function(e) {
  * End Main Functions
  * Begin Events
  *
-*/
+ */
